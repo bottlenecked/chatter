77052bf75d349425165b2cb6ad28ee809dd3b332
bottlenecked <esemmano@gmail.com>
2020-12-23 20:12:27 +0200

    refactor user chat feature

---
+++ assets/js/chat_room.js
@@ -0,0 +1,27 @@
+import socket from "./socket"
+
+let chatRoomTitle = document.getElementById("chat-room-title");
+if (chatRoomTitle) {
+    let chatRoomName = chatRoomTitle.dataset.chatRoomName;
+    let channel = socket.channel(`chat_room:${chatRoomName}`, {})
+
+    let messageForm = document.getElementById("new-message-form");
+    let messageInput = document.getElementById("message");
+    let messageContainer = document.querySelector("[data-role='messages']");
+    messageForm.addEventListener("submit", e => {
+        e.preventDefault();
+        channel.push("new_message", {
+            body: messageInput.value
+        });
+        e.target.reset();
+    });
+
+    channel.on("new_message", payload => {
+        let messageItem = document.createElement("li");
+        messageItem.dataset.role = "message";
+        messageItem.innerText = payload.body;
+        messageContainer.appendChild(messageItem);
+    });
+
+    channel.join();
+}
