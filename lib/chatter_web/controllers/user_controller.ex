7ef1e196eb7e2d20c5d4024b4960e43c66a16c6f
Manos Emmanouilidis <esemmano@gmail.com>
2021-01-15 12:02:11 +0200

    user creation works

--- lib/chatter_web/controllers/user_controller.ex
+++ lib/chatter_web/controllers/user_controller.ex
@@ -1,7 +1,10 @@
 defmodule ChatterWeb.UserController do
   use ChatterWeb, :controller
 
+  alias Doorman.Auth.Secret
+  alias Doorman.Login.Session
   alias Chatter.User
+  alias Chatter.Repo
 
   def new(conn, _params) do
     changeset = User.changeset(%User{}, %{})
@@ -9,5 +12,19 @@ defmodule ChatterWeb.UserController do
   end
 
   def create(conn, %{"user" => params}) do
+    changeset =
+      %User{}
+      |> User.changeset(params)
+      |> Secret.put_session_secret()
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> Session.login(user)
+        |> redirect(to: "/")
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
