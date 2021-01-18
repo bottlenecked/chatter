85db52059d49854e1c5edaeff84b8fbf2df7ac5d
Manos Emmanouilidis <esemmano@gmail.com>
2021-01-18 09:34:26 +0200

    wip: history

--- lib/chatter_web/channels/chat_room_channel.ex
+++ lib/chatter_web/channels/chat_room_channel.ex
@@ -1,13 +1,16 @@
 defmodule ChatterWeb.ChatRoomChannel do
   use ChatterWeb, :channel
 
+  alias Chatter.Chat
+
   def join("chat_room:" <> _room_name, _msg, socket) do
     {:ok, socket}
   end
 
   def handle_in("new_message", payload, socket) do
-    author = socket.assigns.email
+    %{room: room, author: author} = socket.assigns
     final_payload = Map.put(payload, "author", author)
+    Chat.new_message(room, final_payload)
     broadcast(socket, "new_message", final_payload)
     {:noreply, socket}
   end
